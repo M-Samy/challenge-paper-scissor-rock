d50ffe41c4d7815c198d34c6261c2ff5610694ab
n3rd <n3rd@n3rd>
2019-05-04 13:59:44 +0200

    Apply test cases for wrong and right choices for players

---
+++ tests/unit/ChoiceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Challenge\Helpers\RPSHelper;
+use PHPUnit\Framework\TestCase;
+
+class ChoiceTest extends TestCase
+{
+    public $choicesMock = array(
+        'paper' => 'rock',
+        'rock' => 'scissors',
+        'scissors' => 'paper'
+    );
+
+    public function testWrongChoice()
+    {
+        $playerChoiceMock = "MOCK_WRONG_CHOICE";
+        $checkChoiceExist = RPSHelper::check_keys_existence($this->choicesMock, array($playerChoiceMock));
+        $this->assertFalse($checkChoiceExist);
+    }
+
+    public function testRightChoice()
+    {
+        $playerChoiceMock = "rock";
+        $checkChoiceExist = RPSHelper::check_keys_existence($this->choicesMock, array($playerChoiceMock));
+        $this->assertTrue($checkChoiceExist);
+    }
+}
